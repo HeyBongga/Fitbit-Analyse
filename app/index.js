bbe7f5f6e8a6211a3160b5f6327398f404e83ab2
Fabrice <herzogfa@hs-pforzheim.de>
2026-04-15 15:08:04 +0200

    debug

--- app/index.js
+++ app/index.js
@@ -1,15 +1,14 @@
+
 import * as document from "document";
 import { initClock } from "./clock";
 import { initBodyPresence } from "./sensors/bodypresence";
 
-// Get references to the <text> elements in the document
+// Get references to the label elements in the document
 const clockLabel = document.getElementById("clockLabel");
 const bodyPresenceLabel = document.getElementById("bodyPresenceLabel");
 
 // Initialize the clock and body presence sensor with the respective label elements
 initClock(clockLabel);
 initBodyPresence(bodyPresenceLabel);
-
-
 
 
--- app/sensors/bodypresence.js
+++ app/sensors/bodypresence.js
@@ -3,24 +3,19 @@ import { logPrototypeChain } from "../debug/prototype";
 
 // Initialize the body presence sensor and update the label element based on the sensor's readings
 export function initBodyPresence(labelElement) {
-
+  console.log("Started");
   // Create a new instance of the BodyPresenceSensor  
   const bodysensor = new BodyPresenceSensor();
 
+  console.log("before event listener:", bodysensor);
   //logPrototypeChain(bodysensor);
-
-  function updateLabel() {
-    if(bodysensor.present === null) {
-      labelElement.text = "Loading...";
-      return;
-    }
-    labelElement.text = bodysensor.present ? "On-wrist" : "Off-wrist";
-  };
-
   // Add an event listener to the sensor to update the label element whenever a new reading is available
-  bodysensor.addEventListener("reading", () => { updateLabel(); });
+  bodysensor.addEventListener("reading", () => { 
+    labelElement.text = bodysensor.present ? "On-wrist" : "Off-wrist";
+    console.log("In event listener:", bodysensor);
+  });
 
   // Start the sensor to begin receiving readings
   bodysensor.start();
-  updateLabel();
+ console.log("ended");
 }
